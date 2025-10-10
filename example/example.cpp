d7cbd6c9280128a7c89263dbf97339054d57aa05
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-10-10 13:47:38 +0000

    rename test.cc to example.cc

--- example/example.cpp
+++ example/example.cpp
@@ -4,7 +4,7 @@
 //!   See the LICENSE file in the project root for full license information.
 //!
 //! Test file
-//!   Compilation: g++ -std=c++17 -Iinclude/ test/test.cpp -o test.out
+//!   Compilation: g++ -std=c++17 -Iinclude/ example/example.cpp -o example.out
 
 #include <iostream>
 
@@ -13,6 +13,9 @@
 using namespace ansi::graphic;
 
 int main(int argc, char** argv) {
-  std::cout << set_color(Color::Black, Color::White) << set_mode({Mode::Blinking}) << "a" << reset_all() << std::endl;
-
+  std::cout 
+  << set_color(Color::Black, Color::White) << set_mode({Mode::Blinking}) 
+  << "Lore ipsum :)" 
+  << reset_all() << std::endl;
+  return 0;
 }
