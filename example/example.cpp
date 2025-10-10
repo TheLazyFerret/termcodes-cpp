05d65d0e043b1f6028ab078f053c49abbe6e7eb8
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-10-10 16:32:56 +0000

    update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -3,7 +3,7 @@
 //!   Using the license MIT.
 //!   See the LICENSE file in the project root for full license information.
 //!
-//! Test file
+//! Example file
 //!   Compilation: g++ -std=c++17 -Iinclude/ example/example.cpp -o example.out
 
 #include <iostream>
@@ -12,10 +12,25 @@
 
 using namespace ansi::graphic;
 
-int main(int argc, char** argv) {
-  std::cout 
-  << set_color(Color::Black, Color::White) << set_mode({Mode::Blinking}) 
-  << "Lore ipsum :)" 
+int main() {
+  // Set the foreground color to red. 
+  // Notice that you must reset the color, just as like using normal ANSI codes.
+  std::cout << set_color(Color::Red) << "Red" << set_color() << std::endl;
+
+  // Set the foreground color to red and the background to white.
+  std::cout << set_color(Color::Red, Color::White) << "Red over white" 
+  << set_color() << std::endl;
+
+  // Set the underline mode. As same with colors, you need to reset them.
+  std::cout << set_mode({Mode::Underline}) << "Underlined" << reset_mode({Mode::Underline}) << std::endl;
+
+  // When you need to use a color and a mode, you can easily reset them with reset_all()
+  std::cout << set_color(Color::Red) << set_mode({Mode::Italic}) << "red and italic" 
   << reset_all() << std::endl;
+  
+  // You can set multiple modes at the same time. You can set multiple times
+  // the same mode, but only will be applied one time.
+  std::cout << set_color(Color::Cyan) << set_mode({Mode::Italic, Mode::Bold, Mode::Italic}) 
+  << "Cyan, italic and bold" << reset_all() << std::endl;
   return 0;
 }
