bd677ad419503251ca723133758b4b33e54b26cf
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-10-19 09:03:51 +0000

    add cursor example file

---
+++ example/cursor.cpp
@@ -0,0 +1,18 @@
+//! Author TheLazyFerret (https://github.com/TheLazyFerret).
+//! Copyright (c) 2025 TheLazyFerret.
+//!   Using the license MIT.
+//!   See the LICENSE file in the project root for full license information.
+//!
+//! Example cursor file
+//!   Compilation: g++ -Wall -Werror --pedantic -std=c++17 -Iinclude/ example/cursor.cpp -o cursor.out
+
+#include <iostream>
+
+#include "termcodes/ansi.hpp"
+
+using namespace ansi::cursor;
+
+int main() {
+
+  return 0;
+}
